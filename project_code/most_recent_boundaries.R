da8c3c3b2402a2f58ca2f5b7e460cfbedf961851
Alex Miller <alex.k.miller@gmail.com>
2018-07-19 09:29:30 -0400

    Object IDs for India level 1

--- project_code/most_recent_boundaries.R
+++ project_code/most_recent_boundaries.R
@@ -3,6 +3,8 @@ new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"
 if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only=T)
 
+source("https://raw.githubusercontent.com/akmiller01/r_arc_json/master/arc_json.R")
+
 if(.Platform$OS.type == "unix"){
   prefix = "~"
 }else{
@@ -26,4 +28,9 @@ recent_dhs_boundaries = subset(svy_boundaries,cc_year %in% recent)
 save(recent_dhs_boundaries,file="project_data/recent_dhs_boundaries.RData")
 
 recent_dhs_1 = subset(recent_dhs_boundaries,LEVELRNK==1)
+indian_obj_ids = c(4426:4461)
+if(sum(indian_obj_ids %in% recent_dhs_1$OBJECTID,na.rm=T)>0){
+  stop("We've got a non-unique ID.")
+}
+recent_dhs_1$OBJECTID[which(is.na(recent_dhs_1$OBJECTID))] = indian_obj_ids
 save(recent_dhs_1,file="project_data/recent_dhs_1.RData")
